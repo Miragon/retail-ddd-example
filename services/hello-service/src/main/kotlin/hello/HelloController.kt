c8efda9748664ed0a487da011dde9224bebb35de
Andreas Riepl <andreas.riepl@miragon.io>
2025-04-28 09:11:45 +0200

    feat: add hello controller

---
+++ services/hello-service/src/main/kotlin/hello/HelloController.kt
@@ -0,0 +1,11 @@
+package io.miragon.app.hello
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    @GetMapping("/hello")
+    fun sayHello(): String = "Hello, world!"
+}
