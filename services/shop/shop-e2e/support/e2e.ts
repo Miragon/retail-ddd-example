80251f14478aff8370d4b985affa2f95b617f383
Martin Berner <73341627+marber63@users.noreply.github.com>
2025-09-24 10:04:46 +0200

    e2e/use-less-arrow-function (): best practice, function instead arrowFunction (#39)

--- services/shop/shop-e2e/support/e2e.ts
+++ services/shop/shop-e2e/support/e2e.ts
@@ -1,7 +1,7 @@
 // NOTE: Do not import the support file anywhere. It would bring about strange results.
 import "./commands";
 
-Cypress.on("uncaught:exception", (err) => {
+Cypress.on("uncaught:exception", function (err) {
     console.error("Uncaught:Exception", err);
     return false;
 });
